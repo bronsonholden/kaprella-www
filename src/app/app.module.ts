f365ed9a452335afd12b6a0a800c4e5d2e214431
Paul Holden <paul@holdensoftware.com>
2020-07-11 17:50:07 -0700

    Work on field create form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -48,6 +49,7 @@ import { CreateFarmerComponent } from './create/create-farmer/create-farmer.comp
     CreateFarmerComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     HttpClientModule,
     ApiModule,
