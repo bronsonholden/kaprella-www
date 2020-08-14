a741f8c75a85ba18288ac2bf57257c34311f11a3
Paul Holden <paul@holdensoftware.com>
2020-08-13 21:43:13 -0700

    feat(show-licensor): Add basic Licensor profile page

--- src/app/show/show-licensor/show-licensor.component.ts
+++ src/app/show/show-licensor/show-licensor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LicensorApiService } from '../../licensor-api.service';
+import { Licensor } from '../../models/licensor';
 
 @Component({
   selector: 'app-show-licensor',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowLicensorComponent implements OnInit {
 
-  constructor() { }
+  licensor: Licensor;
+  scope: any;
+
+  about = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non neque ut ligula interdum condimentum sit amet nec nibh. Nullam enim tortor, venenatis vitae elit in, cursus vehicula justo. Praesent feugiat vulputate orci. Nam suscipit sapien sit amet nisl tempus sagittis. Praesent metus metus, iaculis non nunc et, interdum tincidunt dolor. Quisque eu erat viverra, iaculis ligula vel, convallis nisi. Suspendisse potenti. Sed imperdiet odio lacinia congue cursus.
+
+Donec hendrerit a sem eu dapibus. Curabitur blandit nec elit sed suscipit. Sed ornare volutpat sapien id fringilla. Phasellus ipsum risus, convallis efficitur lobortis non, ullamcorper a felis.
+`;
+
+  constructor(private licensorApi: LicensorApiService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: any) => {
+      this.scope = {
+        'filter': [ `prop('farmer_id') == ${params.id}` ]
+      };
+
+      this.licensorApi.get(params.id).subscribe((res: any) => {
+        this.licensor = res.data;
+      });
+    });
   }
 
 }
