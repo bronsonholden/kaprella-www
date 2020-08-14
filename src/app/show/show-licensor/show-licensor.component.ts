a1d4bb9391530031c4ba37b8482247bd9dae3a5a
Paul Holden <paul@holdensoftware.com>
2020-08-13 22:00:55 -0700

    stlye(show-licensor): Remove line break at start of sample About

--- src/app/show/show-licensor/show-licensor.component.ts
+++ src/app/show/show-licensor/show-licensor.component.ts
@@ -13,8 +13,7 @@ export class ShowLicensorComponent implements OnInit {
   licensor: Licensor;
   scope: any;
 
-  about = `
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non neque ut ligula interdum condimentum sit amet nec nibh. Nullam enim tortor, venenatis vitae elit in, cursus vehicula justo. Praesent feugiat vulputate orci. Nam suscipit sapien sit amet nisl tempus sagittis. Praesent metus metus, iaculis non nunc et, interdum tincidunt dolor. Quisque eu erat viverra, iaculis ligula vel, convallis nisi. Suspendisse potenti. Sed imperdiet odio lacinia congue cursus.
+  about = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non neque ut ligula interdum condimentum sit amet nec nibh. Nullam enim tortor, venenatis vitae elit in, cursus vehicula justo. Praesent feugiat vulputate orci. Nam suscipit sapien sit amet nisl tempus sagittis. Praesent metus metus, iaculis non nunc et, interdum tincidunt dolor. Quisque eu erat viverra, iaculis ligula vel, convallis nisi. Suspendisse potenti. Sed imperdiet odio lacinia congue cursus.
 
 Donec hendrerit a sem eu dapibus. Curabitur blandit nec elit sed suscipit. Sed ornare volutpat sapien id fringilla. Phasellus ipsum risus, convallis efficitur lobortis non, ullamcorper a felis.
 `;
