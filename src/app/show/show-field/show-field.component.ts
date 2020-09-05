99c695678124296f796325632c2370dbb6724448
Paul Holden <paul@codelunker.com>
2020-09-04 21:28:59 -0700

    feat(show-field): Map loads centered on boundary

--- src/app/show/show-field/show-field.component.ts
+++ src/app/show/show-field/show-field.component.ts
@@ -3,6 +3,10 @@ import { ActivatedRoute } from '@angular/router';
 import { FieldApiService } from '../../field-api.service';
 import { Field } from '../../models/field';
 import { Farmer } from '../../models/farmer';
+import { Coordinate } from '../../maps/geometries/coordinate';
+import { Multipolygon } from '../../maps/geometries/multipolygon';
+import { Polygon } from '../../maps/geometries/polygon';
+import * as wkt from 'terraformer-wkt-parser';
 
 @Component({
   selector: 'app-show-field',
@@ -14,6 +18,20 @@ export class ShowFieldComponent implements OnInit {
   unit = 'ha';
   field: Field;
   farmer: Farmer;
+  multipolygon: Multipolygon;
+
+  options = {
+    zoom: 15,
+    streetViewControl: false,
+    mapTypeControl: true,
+    mapTypeId: 'roadmap',
+    styles: []
+  };
+
+  polygonOptions = {
+    fillColor: '#00ff00',
+    strokeColor: '#00bb00'
+  };
 
   constructor(public fieldApi: FieldApiService,
               private activatedRoute: ActivatedRoute) { }
@@ -23,8 +41,28 @@ export class ShowFieldComponent implements OnInit {
       this.fieldApi.get(params.id).subscribe((res: any) => {
         this.field = res.data;
         this.farmer = res.included[0];
+
+        this.multipolygon = this.loadMultipolygon(this.field.attributes.boundary);
+
+        const geometry = <any>wkt.parse(this.field.meta.centroid);
+
+        this.options.center = {
+          lng: geometry.coordinates[0],
+          lat: geometry.coordinates[1]
+        };
       });
     });
+  }
+
+  loadMultipolygon(boundary) {
+    const geometry = <any>wkt.parse(boundary);
+
+    const polygons = geometry.coordinates.map(polygon => {
+      const loops = polygon.map(loop => loop.map(coord => new Coordinate(coord[1], coord[0])));
+      return new Polygon(loops[0], loops.slice(1));
+    });
+
+    return new Multipolygon(polygons);
   }
 
 }
